36242664d8c4b08eca9be564bc9358a219ae9e00
Emma Adams <eadams36@jhu.edu>
2018-04-30 18:14:24 -0400

    Update extend_inning_line.py

--- Modules/extend_inning_line.py
+++ Modules/extend_inning_line.py
@@ -2,8 +2,8 @@ import pandas as pd
 import sys
 
 def main():
-    bat_type = "line_drive"
-    out_type = "lines_out"
+    bat_type = "line drive"
+    out_type = "lines out"
     extend_inning = 0
     total = 0
     ball = False
